332955ac4dec50d137b01bf1e45055d8d48963c7
Patrick Lacz <patrick.lacz@gmail.com>
2017-10-06 14:53:25 -0600

    Adding many more tests and basic functions on cellids.

--- s2--0.0.1.sql
+++ s2--0.0.1.sql
@@ -5,13 +5,25 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION s2" to load this file. \quit
 
+--
+-- Conversion functions
+--
+
 -- PostgreSQL doesn't handle unsigned ints, so we have to re-interpret the byte representation
 -- of the id.
 CREATE OR REPLACE FUNCTION s2_cellid_from_latlng(lat real, lng real) RETURNS bigint
 AS $$
   import s2sphere
-  id = s2sphere.CellId.from_lat_lng(s2sphere.LatLng.from_degrees(lat, lng)).id()
-  return int.from_bytes(id.to_bytes(8, 'big', signed=False), 'big', signed=True)
+  id = s2sphere.CellId.from_lat_lng(s2sphere.LatLng.from_degrees(lat, lng))
+  return int.from_bytes(id.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+-- Convert from token (text) to cellid (long) representation.
+CREATE OR REPLACE FUNCTION s2_cellid_from_token(token text) RETURNS bigint
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return int.from_bytes(id.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
 $$ LANGUAGE plpython3u IMMUTABLE STRICT;
 
 -- Get the string token for a cellid from the latitude and longitude
@@ -26,41 +38,163 @@ $$ LANGUAGE plpython3u IMMUTABLE STRICT;
 CREATE OR REPLACE FUNCTION s2_token_from_cellid(cellid bigint) RETURNS text
 AS $$
   import s2sphere
-  id = int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False)
-  return s2sphere.CellId(id).to_token()
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.to_token()
 $$ LANGUAGE plpython3u IMMUTABLE STRICT;
 
 -- Get the latitude and longitude as degrees from a cellid
 CREATE OR REPLACE FUNCTION s2_latlng_from_cellid(cellid bigint, OUT lat real, OUT lng real)
 AS $$
   import s2sphere
-  id = int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False)
-  latlng = s2sphere.CellId(id).to_lat_lng()
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  latlng = id.to_lat_lng()
   return (latlng.lat().degrees, latlng.lng().degrees)
 $$ LANGUAGE plpython3u IMMUTABLE STRICT;
 
+-- Get the latitude and longitude as degrees from a token 
+CREATE OR REPLACE FUNCTION s2_latlng_from_token(token text, OUT lat real, OUT lng real)
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  latlng = id.to_lat_lng()
+  return (latlng.lat().degrees, latlng.lng().degrees)
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+--
+-- CellId based functions
+--
+
+-- Return whether this is a valid s2 cellid.
+CREATE OR REPLACE FUNCTION s2_cellid_is_valid(cellid bigint) RETURNS boolean
+AS $$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.is_valid()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_cellid_is_leaf(cellid bigint) RETURNS boolean
+AS $$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.is_leaf()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
 
 -- Return the level of the cellid
 CREATE OR REPLACE FUNCTION s2_cellid_level(cellid bigint) RETURNS int
 AS $$
   import s2sphere
-  id = int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False)
-  return s2sphere.CellId(id).level()
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.level()
 $$ LANGUAGE plpython3u IMMUTABLE STRICT;
 
 -- Return a cell id at a parent level for the passed in cell id
 CREATE OR REPLACE FUNCTION s2_cellid_parent(cellid bigint, parent_level int) RETURNS bigint
 AS $$
   import s2sphere
-  id = int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False)
-  newid = s2sphere.CellId(id).parent(parent_level)
-  return int.from_bytes(newid.to_bytes(8, 'big', signed=False), 'big', signed=True)
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  newid = id.parent(parent_level)
+  return int.from_bytes(newid.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
 $$ LANGUAGE plpython3u IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION s2_cellid_children(cellid bigint) RETURNS SETOF bigint
 AS $$
   import s2sphere
-  id = int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False)
-  for child in s2sphere.CellId(id).children():
-    yield int.from_bytes(child.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  if not id.is_leaf():
+    for child in id.children():
+      yield int.from_bytes(child.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
 $$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_cellid_edge_neighbors(cellid bigint) RETURNS SETOF bigint
+AS $$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  for neighbor in id.get_edge_neighbors():
+    yield int.from_bytes(neighbor.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_cellid_contains(a bigint, b bigint) RETURNS boolean
+AS $$
+  import s2sphere
+  id_a = s2sphere.CellId(int.from_bytes(a.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  id_b = s2sphere.CellId(int.from_bytes(b.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id_a.contains(id_b)
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_cellid_intersects(a bigint, b bigint) RETURNS boolean
+AS $$
+  import s2sphere
+  id_a = s2sphere.CellId(int.from_bytes(a.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  id_b = s2sphere.CellId(int.from_bytes(b.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id_a.intersects(id_b)
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+--
+-- Token based functions
+--
+
+-- Return whether this is a valid s2 cellid.
+CREATE OR REPLACE FUNCTION s2_token_is_valid(token text) RETURNS boolean
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return id.is_valid()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_token_is_leaf(token text) RETURNS boolean
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return id.is_leaf()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+-- Return the level of the cellid
+CREATE OR REPLACE FUNCTION s2_token_level(token text) RETURNS int
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return id.level()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+-- Return a cell id at a parent level for the passed in cell id
+CREATE OR REPLACE FUNCTION s2_token_parent(token text, parent_level int) RETURNS text
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  newid = id.parent(parent_level)
+  return newid.to_token()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_token_children(token text) RETURNS SETOF text
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  if not id.is_leaf():
+    for child in id.children():
+      yield child.to_token()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_token_edge_neighbors(token text) RETURNS SETOF text
+AS $$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  for neighbor in id.get_edge_neighbors():
+    yield neighbor.to_token()
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_token_contains(a text, b text) RETURNS boolean
+AS $$
+  import s2sphere
+  id_a = s2sphere.CellId.from_token(a)
+  id_b = s2sphere.CellId.from_token(b)
+  return id_a.contains(id_b)
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION s2_token_intersects(a text, b text) RETURNS boolean
+AS $$
+  import s2sphere
+  id_a = s2sphere.CellId.from_token(a)
+  id_b = s2sphere.CellId.from_token(b)
+  return id_a.intersects(id_b)
+$$ LANGUAGE plpython3u IMMUTABLE STRICT;
+
--- sql/s2_test.sql
+++ sql/s2_test.sql
@@ -1,8 +1,99 @@
 CREATE LANGUAGE plpython3u;
 CREATE EXTENSION s2;
 
+--CREATE OR REPLACE FUNCTION s2_cellid_from_latlng(lat real, lng real) RETURNS bigint
 SELECT s2_cellid_from_latlng(0, 0);
 SELECT s2_cellid_from_latlng(40.5, -105.5);
 
+--CREATE OR REPLACE FUNCTION s2_latlng_from_cellid(cellid bigint, OUT lat real, OUT lng real)
+SELECT s2_latlng_from_cellid(-8689295774057980903);
+
+SELECT s2_cellid_parent(-8689295774057980903, 20);
+SELECT s2_cellid_parent(-8689295774057980903, 1);
+
+SELECT s2_cellid_level(s2_cellid_from_latlng(40.5, -105.5));
+SELECT s2_cellid_level(s2_cellid_parent(s2_cellid_from_latlng(40.5, -105.5), 21));
+
+SELECT s2_cellid_children(-8689295774058545152);
+
+--CREATE OR REPLACE FUNCTION s2_token_from_latlng(lat real, lng real) RETURNS text
 SELECT s2_token_from_latlng(0, 0);
 SELECT s2_token_from_latlng(40.5, -105.5);
+
+--CREATE OR REPLACE FUNCTION s2_token_from_cellid(cellid bigint) RETURNS text
+SELECT s2_token_from_cellid(0);
+SELECT s2_token_from_cellid(s2_cellid_from_latlng(40.5, -105.5));
+SELECT s2_token_from_cellid(s2_cellid_parent(s2_cellid_from_latlng(40.5, -105.5), 10));
+
+--CREATE OR REPLACE FUNCTION s2_cellid_from_token(token text) RETURNS bigint
+SELECT s2_cellid_from_token('');
+SELECT s2_cellid_from_token('zzz'); -- Note this currently causes an error, not id 0
+SELECT s2_cellid_from_token('87696b8806f89c19');
+SELECT s2_cellid_from_token('8');
+
+--CREATE OR REPLACE FUNCTION s2_latlng_from_token(token text, OUT lat real, OUT lng real)
+SELECT s2_latlng_from_token('87696b8806f89c19');
+
+--CREATE OR REPLACE FUNCTION s2_cellid_is_valid(cellid bigint) RETURNS int
+SELECT s2_cellid_is_valid(-8689295774058545152);
+
+--CREATE OR REPLACE FUNCTION s2_cellid_is_leaf(cellid bigint) RETURNS int
+SELECT s2_cellid_is_leaf(s2_cellid_from_latlng(40.5, -105.5));
+SELECT s2_cellid_is_leaf(s2_cellid_parent(-8689295774058545152, 15));
+
+--CREATE OR REPLACE FUNCTION s2_cellid_level(cellid bigint) RETURNS int
+SELECT s2_cellid_level(-8689295774058545152);
+
+--CREATE OR REPLACE FUNCTION s2_cellid_parent(cellid bigint, parent_level int) RETURNS bigint
+SELECT s2_cellid_parent(-8689295774058545152, 15);
+
+--CREATE OR REPLACE FUNCTION s2_cellid_children(cellid bigint) RETURNS SETOF bigint
+SELECT s2_cellid_children(-8689295774058545152);
+SELECT s2_cellid_children(s2_cellid_from_latlng(40.5, -105.5));
+SELECT s2_cellid_children(s2_cellid_parent(-8689295774058545152, 15));
+
+--CREATE OR REPLACE FUNCTION s2_cellid_edge_neighbors(cellid bigint) RETURNS SETOF bigint
+SELECT s2_cellid_edge_neighbors(-8689295774058545152);
+
+--CREATE OR REPLACE FUNCTION s2_cellid_contains(a bigint, b bigint) RETURNS bool
+SELECT s2_cellid_contains(-8689295774058545152, -8689295774058545152);
+SELECT s2_cellid_contains(-8689295774058545152, s2_cellid_parent(-8689295774058545152, 20));
+SELECT s2_cellid_contains(s2_cellid_parent(-8689295774058545152, 20), -8689295774058545152);
+
+--CREATE OR REPLACE FUNCTION s2_cellid_intersects(a bigint, b bigint) RETURNS bool
+SELECT s2_cellid_intersects(-8689295774058545152, -8689295774058545152);
+SELECT s2_cellid_contains(-8689295774058545152, s2_cellid_parent(-8689295774058545152, 20));
+SELECT s2_cellid_contains(s2_cellid_parent(-8689295774058545152, 20), -8689295774058545152);
+
+--CREATE OR REPLACE FUNCTION s2_token_is_valid(token text) RETURNS bool
+SELECT s2_token_is_valid('87696b8806f89c19');
+SELECT s2_token_is_valid('8769b');
+
+--CREATE OR REPLACE FUNCTION s2_token_is_leaf(token text) RETURNS bool
+SELECT s2_token_is_leaf('87696b8806f89c19');
+SELECT s2_token_is_leaf('8769b');
+
+--CREATE OR REPLACE FUNCTION s2_token_level(token text) RETURNS int
+SELECT s2_token_level('87696b8806f89c19');
+SELECT s2_token_level('8769b');
+
+--CREATE OR REPLACE FUNCTION s2_token_parent(token text, parent_level int) RETURNS text
+SELECT s2_token_parent('87696b8806f89c19', 15);
+SELECT s2_token_parent('8769b', 5);
+
+--CREATE OR REPLACE FUNCTION s2_token_children(token text) RETURNS SETOF text
+SELECT s2_token_children('87696b8806f89c19');
+SELECT s2_token_children('8769b');
+
+--CREATE OR REPLACE FUNCTION s2_token_edge_neighbors(token text) RETURNS SETOF text
+SELECT s2_token_edge_neighbors('87696b8806f89c19');
+SELECT s2_token_edge_neighbors('87696b');
+
+--CREATE OR REPLACE FUNCTION s2_token_contains(a text, b text) RETURNS bool
+SELECT s2_token_contains('87696b8806f89c19', '87696b');
+SELECT s2_token_contains('87696b', '87696b8806f89c19');
+
+--CREATE OR REPLACE FUNCTION s2_token_intersects(a text, b text) RETURNS bool
+SELECT s2_token_intersects('87696b8806f89c19', '87696b');
+SELECT s2_token_intersects('87696b', '87696b8806f89c19');
+
